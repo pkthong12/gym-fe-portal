e5305d7f86d9ef1e56a85af270e03aac2a0e3830
pkthong12 <pkthong12@gmail.com>
2024-05-12 03:48:03 +0700

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),provideHttpClient(withFetch())]//, provideClientHydration() cmt tạm thời tránh lỗi
 };
 
-export const baseUrl: string = 'https://localhost:44360';
-// export const baseUrl: string = 'http://cuzemm-001-site1.etempurl.com'; // BUILD PRODUCT WITH THIS API
+// export const baseUrl: string = 'https://localhost:44360';
+export const baseUrl: string = 'http://cuzemm-001-site1.etempurl.com'; // BUILD PRODUCT WITH THIS API
 export const language: boolean = false;
 
